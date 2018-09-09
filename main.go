e85d2f8a0d33527159ab27ed113478c1a2929a82
Ivan Golikov <igolikov@octoberry.ru>
2018-09-09 15:45:40 +0300

    --wip-- [skip ci]

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -12,18 +13,36 @@ var (
 	flagPort = flag.String("port", "8082", "Port to listen on")
 )
 
-var results []string
+type WebhookCallback struct {
+	PushData struct {
+		Tag string `json:"tag"`
+	} `json:"push_data"`
+
+	Repository struct {
+		Name string `json:"name"`
+	} `json:"repository"`
+}
 
 func PostHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
+		var webhook WebhookCallback
 		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			fmt.Println("error readall")
+		}
+		if err := json.Unmarshal(body, &webhook); err != nil {
+			fmt.Printf("There was an error encoding the json. err = %s", err)
+		}
 		if err != nil {
 			http.Error(w, "Error reading request body",
 				http.StatusInternalServerError)
 		}
-		results = append(results, string(body))
-
 		fmt.Fprint(w, "POST done")
+		fmt.Println(webhook.PushData.Tag)
+		fmt.Println(webhook.Repository.Name)
+		//		cmdStr := "docker run -v ~/exp/a.out:/a.out ubuntu:14.04 /a.out -m 10m"
+		//		out, _ := exec.Command("/bin/sh", "-c", cmdStr).Output()
+		//		fmt.Printf("%s", out)
 	} else {
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 	}
