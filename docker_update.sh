0c1e6de0ca235da47d4e1c83ffa2a7a402531403
Ivan Golikov <igolikov@octoberry.ru>
2018-10-28 09:48:20 +0300

    Updated env file paths

--- docker_update.sh
+++ docker_update.sh
@@ -2,11 +2,14 @@
 imageName=$1:$2
 containerName=autodeployed-$3-$2
 PORTS_PAIR=""
+ENV_FILE=""
 
 if [[ $2 == "master" ]]; then
 	PORTS_PAIR="80:8080"
+	ENV_FILE="prod_env.list"
 else
 	PORTS_PAIR="8080:8080"
+	ENV_FILE="env.list"
 fi
 
 docker build -t $imageName -f Dockerfile  .
@@ -15,4 +18,4 @@ echo Stop and delete old container...
 docker stop $containerName && docker rm -f $containerName && docker rmi $imageName
 
 echo Run new container...
-docker run -d --env-file env.list --restart=always -p $PORTS_PAIR --name $containerName $imageName
+docker run -d --env-file $ENV_FILE --restart=always -p $PORTS_PAIR --name $containerName $imageName
