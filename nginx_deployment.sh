c3bd37cebb7fc01441ffb3c168ea409819a4588f
Ivan Golikov <igolikov@octoberry.ru>
2019-05-22 16:10:41 +0300

    Updated nginx deployment

--- nginx_deployment.sh
+++ nginx_deployment.sh
@@ -3,4 +3,4 @@ docker stop swadeshness-nginx
 
 docker rm swadeshness-nginx 
 docker rmi aeternas/swadeshness-nginx 
-docker run -v swadeshness-certs:/etc/nginx/certs/ -p 8080:8080 -p 443:443 -p 8084:8084 -p 8086:8086 -d --restart=always --name swadeshness-nginx --network swadeshness aeternas/swadeshness-nginx
+docker run -v swadeshness-certs:/etc/nginx/certs/ -p 8080:8080 -p 443:443 -p 8084:8084 -p 8086:8086 -p 81:81 -d --restart=always --name swadeshness-nginx --network swadeshness aeternas/swadeshness-nginx
