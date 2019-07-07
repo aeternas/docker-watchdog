a20a6eae3faac0f7b8f9f871838810ebf5b35d6b
Ivan Golikov <igolikov@octoberry.ru>
2019-07-07 19:55:00 +0200

    Refactored listener a bit

--- main.go
+++ main.go
@@ -23,44 +23,43 @@ var (
 )
 
 func PostHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		var webhook WebhookCallback
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Println("error on ReadAll: ", err)
-		}
-		if err := json.Unmarshal(body, &webhook); err != nil {
-			log.Printf("There was an error encoding the json. err = %s", err)
-			http.Error(w, "Error reading request body", http.StatusInternalServerError)
-		}
-		log.Println("POST done: ", w)
-		log.Println("TAG is ", webhook.PushData.Tag)
-		log.Println("RepoName is ", webhook.Repository.RepoName)
-		log.Println("Repository.Name is ", webhook.Repository.Name)
-		imageName := fmt.Sprintf("%s:%s", webhook.Repository.RepoName, webhook.PushData.Tag)
-		log.Println(imageName)
-		var cmdStr string
-		if webhook.Repository.Name == "swadeshness-nginx" {
-			cmdStr = fmt.Sprintf("./nginx_deployment.sh %s %s %s", webhook.Repository.RepoName, webhook.PushData.Tag, webhook.Repository.Name)
-		} else if webhook.Repository.Name == "swadeshness-spring" {
-			cmdStr = fmt.Sprintf("./spring_deployment.sh %s %s %s", webhook.Repository.RepoName, webhook.PushData.Tag, webhook.Repository.Name)
-		} else {
-			cmdStr = fmt.Sprintf("./docker_update.sh %s %s %s", webhook.Repository.RepoName, webhook.PushData.Tag, webhook.Repository.Name)
-		}
-		log.Println(cmdStr)
-		out, err := exec.Command("/bin/bash", "-c", cmdStr).Output()
-		if err != nil {
-			log.Println("error: ", err)
-			text := fmt.Sprintf("{\"attachments\":[{\"fallback\":\"Failed to deploy application\",\"color\":\"#ff0000\",\"author_name\":\"Docker-Watchdog\",\"title\":\"Deploy result\",\"text\":\"Failed to deploy application: %s:%s with error %s\"}]}", webhook.Repository.RepoName, webhook.PushData.Tag, err)
-			deploymentResult(text)
-		} else {
-			text := fmt.Sprintf("{\"attachments\":[{\"fallback\":\"Application has been successfully deployed\",\"color\":\"#36a64f\",\"author_name\":\"Docker-Watchdog\",\"title\":\"Deploy result\",\"text\":\"Application has been successfully deployed: %s:%s\"}]}", webhook.Repository.RepoName, webhook.PushData.Tag)
-			deploymentResult(text)
-		}
-		log.Println(out)
-	} else {
+	if r.Method != http.MethodPost {
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 	}
+	var webhook WebhookCallback
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println("error on ReadAll: ", err)
+	}
+	if err := json.Unmarshal(body, &webhook); err != nil {
+		log.Printf("There was an error encoding the json. err = %s", err)
+		http.Error(w, "Error reading request body", http.StatusInternalServerError)
+	}
+	log.Println("POST done: ", w)
+	log.Println("TAG is ", webhook.PushData.Tag)
+	log.Println("RepoName is ", webhook.Repository.RepoName)
+	log.Println("Repository.Name is ", webhook.Repository.Name)
+	imageName := fmt.Sprintf("%s:%s", webhook.Repository.RepoName, webhook.PushData.Tag)
+	log.Println(imageName)
+	var cmdStr string
+	if webhook.Repository.Name == "swadeshness-nginx" {
+		cmdStr = fmt.Sprintf("./nginx_deployment.sh %s %s %s", webhook.Repository.RepoName, webhook.PushData.Tag, webhook.Repository.Name)
+	} else if webhook.Repository.Name == "swadeshness-spring" {
+		cmdStr = fmt.Sprintf("./spring_deployment.sh %s %s %s", webhook.Repository.RepoName, webhook.PushData.Tag, webhook.Repository.Name)
+	} else {
+		cmdStr = fmt.Sprintf("./docker_update.sh %s %s %s", webhook.Repository.RepoName, webhook.PushData.Tag, webhook.Repository.Name)
+	}
+	log.Println(cmdStr)
+	out, err := exec.Command("/bin/bash", "-c", cmdStr).Output()
+	if err != nil {
+		log.Println("error: ", err)
+		text := fmt.Sprintf("{\"attachments\":[{\"fallback\":\"Failed to deploy application\",\"color\":\"#ff0000\",\"author_name\":\"Docker-Watchdog\",\"title\":\"Deploy result\",\"text\":\"Failed to deploy application: %s:%s with error %s\"}]}", webhook.Repository.RepoName, webhook.PushData.Tag, err)
+		deploymentResult(text)
+	} else {
+		text := fmt.Sprintf("{\"attachments\":[{\"fallback\":\"Application has been successfully deployed\",\"color\":\"#36a64f\",\"author_name\":\"Docker-Watchdog\",\"title\":\"Deploy result\",\"text\":\"Application has been successfully deployed: %s:%s\"}]}", webhook.Repository.RepoName, webhook.PushData.Tag)
+		deploymentResult(text)
+	}
+	log.Println(out)
 }
 
 func deploymentResult(s string) {
